7ae6776696bb834dc6d0cb55ab66f2eec2cdf254
Falon Darville <falondarville@gmail.com>
2021-05-30 18:57:17 -0700

    add body parser

--- index.js
+++ index.js
@@ -2,9 +2,26 @@ const express = require('express');
 
 const app = express();
 
+// Middleware: parse form body 
+app.use(express.urlencoded({ extended: true }));
+
 // root route
 app.get('/', (req, res) => {
-    res.send('hi there!');
+    res.send(`
+        <div>
+            <form method="POST">
+                <input name="email" placeholder="email" />
+                <input name="password" placeholder="password" />
+                <input name="passwordConfirmation" placeholder="password confirmation" />
+                <button>Sign Up</button>
+            </form>
+        </div>
+    `);
+});
+
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.send('Account created');
 });
 
 // listen on port 3000
