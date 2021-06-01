67536be638f5143f820109e448e3f2e14cf575b4
Falon Darville <falondarville@gmail.com>
2021-05-31 21:36:27 -0700

    add checks on form

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const UsersRepo = require('./repositories/users');
 
 const app = express();
 
@@ -19,7 +20,19 @@ app.get('/', (req, res) => {
     `);
 });
 
-app.post('/', (req, res) => {
+app.post('/', async (req, res) => {
+    // destructure keys from body
+    const { email, password, passwordConfirmation} = req.body;
+
+    const existingUser = await UsersRepo.getOneBy({ email });
+    if(existingUser){
+        return res.send('Email in use.');
+    }
+
+    if(password !== passwordConfirmation) {
+        return res.send('Password and password confirmation do not match.');
+    }
+
     console.log(req.body);
     res.send('Account created');
 });
